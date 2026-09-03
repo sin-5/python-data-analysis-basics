ba5adcc72069ec3f725b1774492cd45fe57d253c
Shingo <169685575+sin-5@users.noreply.github.com>
2026-09-03 19:13:34 +0900

    change the way to show the average lap time

--- lap_time_basics.py
+++ lap_time_basics.py
@@ -3,4 +3,9 @@ lap_times = [92.4, 91.8, 93.1, 91.6, 92.0]
 for i in range(len(lap_times)):
     print('Lap', i + 1, lap_times[i])
 
-print('Average:', sum(lap_times) / len(lap_times))
+#print('Average:', sum(lap_times) / len(lap_times))
+SUM = 0
+for i in range(len(lap_times)):
+    SUM += lap_times[i]
+AVERAGE = SUM / len(lap_times)
+print('Average:', AVERAGE)
