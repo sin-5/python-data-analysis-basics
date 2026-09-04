7ead0c6094ca2db8d5948c952ffdbe6eb97adb60
Shingo <169685575+sin-5@users.noreply.github.com>
2026-09-05 01:50:51 +0900

    move finding fastest lap into function

--- lap_time_basics.py
+++ lap_time_basics.py
@@ -4,22 +4,26 @@ for i in range(len(lap_times)):
     print('Lap', i + 1, lap_times[i])
 
 #print('Average:', sum(lap_times) / len(lap_times))
-def average_lap_time(lt):
+def average_lap_time(lap_times):
     total = 0
-    for i in range(len(lt)):
-        total += lt[i]
-    average = total / len(lt)
+    for i in range(len(lap_times)):
+        total += lap_times[i]
+    average = total / len(lap_times)
     return average
 
 print('Average:', average_lap_time(lap_times))
 
-fastest = lap_times[0]
-fastest_lap = 1
-for i in range(len(lap_times)):
-    if fastest >= lap_times[i]:
-        fastest = lap_times[i]
-        fastest_lap = i + 1
-print('Fastest:', fastest, 'Lap:', fastest_lap)
+def find_fastest_lap(lap_times):
+    fastest = lap_times[0]
+    fastest_lap = 1
+    for i in range(len(lap_times)):
+        if fastest >= lap_times[i]:
+            fastest = lap_times[i]
+            fastest_lap = i + 1
+    return fastest, fastest_lap
+
+fastest = find_fastest_lap(lap_times)
+print('Fastest:', fastest[0], 'Lap:', fastest[1])
 
 for i in range(len(lap_times)):
     if lap_times[i] < 92:
