3e83ce8574cc813b84b538ed0bb9c0e348e4679d
Shingo <169685575+sin-5@users.noreply.github.com>
2026-09-23 20:14:34 +0900

    learn csv module

---
+++ csv_basics.py
@@ -0,0 +1,17 @@
+import csv
+
+lap_times = {
+    1: 92.4,
+    2: 91.8,
+    3: 93.1,
+    4: 91.6,
+    5: 92.0
+}
+
+with open('lap_times.csv', 'w', newline='') as f:
+    fieldnames = ['lap', 'lap_time']
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
+    for lap_num, lap_time in lap_times.items():
+        writer.writerow({'lap':lap_num, 'lap_time':lap_time})
+    
